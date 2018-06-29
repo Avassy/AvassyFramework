0a6d8b29f5c5a90870e0828b452a9efc0899d3e9
Brecht Baekelandt <brecht@avassy.com>
2018-06-29 21:51:09 +0200

    Unnecessary using.

--- Tests/Avassy.AspNetCore.Mvc.ActionFilters.Tests/Avassy.AspNetCore.Mvc.ActionFilters.Tests/UnitTests/ExcludeUserAgentsActionFilterAttributeTests.cs
+++ Tests/Avassy.AspNetCore.Mvc.ActionFilters.Tests/Avassy.AspNetCore.Mvc.ActionFilters.Tests/UnitTests/ExcludeUserAgentsActionFilterAttributeTests.cs
@@ -1,5 +1,4 @@
-﻿using Avassy.AspNetCore.Mvc.ActionFilters;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
