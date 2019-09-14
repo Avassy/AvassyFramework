3630832f7bfbf030381cb811551dbe6733674858
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2019-09-14 19:10:12 +0200

    Added FromBase64 and ToCamelCase string extensions.

--- Avassy.NetCore.Global.Extensions/Avassy.NetCore.Global.Extensions/StringExtensions.cs
+++ Avassy.NetCore.Global.Extensions/Avassy.NetCore.Global.Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Buffers.Text;
+using System.Linq;
 using System.Text;
 
 namespace Avassy.NetCore.Global.Extensions
@@ -17,6 +18,11 @@ namespace Avassy.NetCore.Global.Extensions
         /// <returns>A cleaned string.</returns>
         public static string RemoveSpecialCharactersAndSpaces(this string str, char charToReplaceSpace = '-')
         {
+            if (str == null)
+            {
+                return null;
+            }
+
             var sb = new StringBuilder();
 
             foreach (var c in str)
@@ -38,9 +44,53 @@ namespace Avassy.NetCore.Global.Extensions
         /// <returns>A base64 encoded string.</returns>
         public static string ToBase64(this string str)
         {
+            if (str == null)
+            {
+                return null;
+            }
+
             var plainTextBytes = Encoding.UTF8.GetBytes(str);
 
             return Convert.ToBase64String(plainTextBytes);
+        }
+
+        /// <summary>
+        /// Converts a base64 encode string to a simple string.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>A simple string.</returns>
+        public static string FromBase64(this string str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            var base64Bytes = Convert.FromBase64String(str);
+
+            return Encoding.ASCII.GetString(base64Bytes);
+        }
+
+        /// <summary>
+        /// Converts a PascalCase string to a camelCase string.
+        /// </summary>
+        /// <param name="str">The PascalCase string.</param>
+        /// <returns>A camelCase string.</returns>
+        public static string ToCamelCase(this string str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            if (!str.Any())
+            {
+                return string.Empty;
+            }
+
+            var firstLetter = str.FirstOrDefault();
+
+            return str.Remove(0, 1).Prepend(firstLetter.ToString().ToLowerInvariant().First()).ToString();
         }
     }
 }
