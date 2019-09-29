4ec6104b9ccb24c6c8cd9e855395620905f495e5
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2019-09-29 17:18:35 +0200

    Added Avassy.NetCore.Global.Attributes.

---
+++ Avassy.NetCore.Global.Attributes/EmailAddressExtendedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Avassy.NetCore.Global.Attributes
+{
+    public class EmailAddressExtendedAttribute : ValidationAttribute
+    {
+        public EmailAddressExtendedAttribute()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return !Regex.IsMatch((string)value, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$") ? new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName)) : null;
+        }
+    }
+}
---
+++ Avassy.NetCore.Global.Attributes/UrlExtendedAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Avassy.NetCore.Global.Attributes
+{
+    public class UrlExtendedAttribute : ValidationAttribute
+    {
+        public UrlExtendedAttribute()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return !Regex.IsMatch((string)value, @"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)") ? new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName)) : null;
+        }
+    }
+}
