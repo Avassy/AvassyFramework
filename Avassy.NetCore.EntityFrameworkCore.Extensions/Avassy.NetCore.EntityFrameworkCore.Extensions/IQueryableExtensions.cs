22d2fcab108be45e8970b2532d18c783b32267a7
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2019-10-03 19:44:51 +0200

    Added Avassy.NetCore.EntityFrameworkCore.Extensions.

---
+++ Avassy.NetCore.EntityFrameworkCore.Extensions/Avassy.NetCore.EntityFrameworkCore.Extensions/IQueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Avassy.NetCore.EntityFrameworkCore.Extensions
+{
+    /// <summary>
+    /// This class contains some IQueryable extension methods.
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        public static IIncludableQueryable<TPreviousProperty, IEnumerable<TProperty>> ToIncludableQueryable<TPreviousProperty, TProperty>(this IQueryable<TProperty> query)
+        {
+            return query as IIncludableQueryable<TPreviousProperty, IEnumerable<TProperty>> ??
+                   throw new InvalidOperationException(
+                       "Cannot convert this IQueryable<T> ta an IIncludableQueryable<TPreviousProperty, IEnumerable<TProperty>>.");
+        }
+    }
+}
---
+++ Avassy.NetCore.EntityFrameworkCore.Extensions/README.md
@@ -0,0 +1,10 @@
+# Avassy.NetCore.EntityFrameworkCore.Extensions
+
+See [http://www.avassy.com/framework/components/Avassy.NetCore.EntityFrameworkCore.Extensions](http://www.avassy.com/framework/components/Avassy.EntityFrameworkCore.Global.Extensions) for more info.
+
+## Classes
+
+- `Avassy.NetCore.Global.Extensions.QueryableExtensions`
+
+## Usage
+
