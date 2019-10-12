4e4cdadb4977432ab4dcb082a1274488ce493f71
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2019-10-12 17:53:52 +0200

    Added override extensions for CooikieAuthenticationOptions.

---
+++ Avassy.AspNetCore.Mvc.Extensions/Avassy.AspNetCore.Mvc.Extensions/CookieAuthenticationOptionsExtensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Avassy.AspNetCore.Mvc.Extensions
+{
+    public static class CookieAuthenticationOptionsExtensions
+    {
+        public static void OverrideRedirectToAccessDeniedByStatusCode(this CookieAuthenticationOptions options, HttpStatusCode statusCode, params string[] urlsToOverride)
+        {
+            options.Events.OnRedirectToAccessDenied = context =>
+            {
+                var mustOverride = false;
+
+                foreach (var url in urlsToOverride)
+                {
+                    mustOverride = context.Request.Path.StartsWithSegments($"{(url.StartsWith('/') ? url : $"/{url}")}");
+
+                    if (mustOverride)
+                    {
+                        break;
+                    }
+                }
+
+                if (mustOverride)
+                {
+                    context.Response.StatusCode = (int)statusCode;
+                }
+
+                return Task.CompletedTask;
+            };
+        }
+
+        public static void OverrideRedirectToLoginByStatusCode(this CookieAuthenticationOptions options, HttpStatusCode statusCode, params string[] urlsToOverride)
+        {
+            options.Events.OnRedirectToLogin = context =>
+            {
+                var mustOverride = false;
+
+                foreach (var url in urlsToOverride)
+                {
+                    mustOverride = context.Request.Path.StartsWithSegments($"{(url.StartsWith('/') ? url : $"/{url}")}");
+
+                    if (mustOverride)
+                    {
+                        break;
+                    }
+                }
+
+                if (mustOverride)
+                {
+                    context.Response.StatusCode = (int)statusCode;
+                }
+
+                return Task.CompletedTask;
+            };
+        }
+
+        public static void OverrideRedirectToLogoutByStatusCode(this CookieAuthenticationOptions options, HttpStatusCode statusCode, params string[] urlsToOverride)
+        {
+            options.Events.OnRedirectToLogout = context =>
+            {
+                var mustOverride = false;
+
+                foreach (var url in urlsToOverride)
+                {
+                    mustOverride = context.Request.Path.StartsWithSegments($"{(url.StartsWith('/') ? url : $"/{url}")}");
+
+                    if (mustOverride)
+                    {
+                        break;
+                    }
+                }
+
+                if (mustOverride)
+                {
+                    context.Response.StatusCode = (int)statusCode;
+                }
+
+                return Task.CompletedTask;
+            };
+        }
+        public static void OverrideRedirectToReturnUrlByStatusCode(this CookieAuthenticationOptions options, HttpStatusCode statusCode, params string[] urlsToOverride)
+        {
+            options.Events.OnRedirectToReturnUrl = context =>
+            {
+                var mustOverride = false;
+
+                foreach (var url in urlsToOverride)
+                {
+                    mustOverride = context.Request.Path.StartsWithSegments($"{(url.StartsWith('/') ? url : $"/{url}")}");
+
+                    if (mustOverride)
+                    {
+                        break;
+                    }
+                }
+
+                if (mustOverride)
+                {
+                    context.Response.StatusCode = (int)statusCode;
+                }
+
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
