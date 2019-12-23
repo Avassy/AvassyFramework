b9e578ebbf06c0faf4088c0169f25846f989d888
Brecht Baekelandt <brecht@avassy.com>
2019-12-23 19:24:51 +0100

    Added GA disabler logic.

---
+++ Avassy.AspNetCore.Mvc.Extensions/Avassy.AspNetCore.Mvc.Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avassy.AspNetCore.Mvc.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static bool ShouldDisableGA<dynamic>(this IHtmlHelper<dynamic> helper)
+        {
+            helper.ViewContext.RouteData.Values.TryGetValue("DisableGA", out var shouldDisableGA);
+
+            return (bool?)shouldDisableGA ?? false;
+        }
+    }
+}
